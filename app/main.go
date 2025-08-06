efa9cff2b9dd5ba55e8cbd9baec9b8dcac8bffa7
Adrian Grzemski <adrian.grzemski@gmail.com>
2025-08-06 11:50:43 +0200

    Refactor main function for better input handling and update debug setting in configuration

--- app/main.go
+++ app/main.go
@@ -8,17 +8,19 @@ import (
 )
 
 func main() {
-	if _, err := fmt.Fprint(os.Stdout, "$ "); err != nil {
-		panic(err)
+	for {
+		if _, err := fmt.Fprint(os.Stdout, "$ "); err != nil {
+			panic(err)
+		}
+
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		// Wait for user input
+		if err != nil {
+			panic(err)
+		}
+
+		input = strings.TrimSpace(input)
+
+		fmt.Printf("%s: command not found\n", input)
 	}
-
-	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	// Wait for user input
-	if err != nil {
-		panic(err)
-	}
-
-	input = strings.TrimSpace(input)
-
-	fmt.Printf("%s: command not found", input)
 }
