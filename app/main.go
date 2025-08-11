573a076df606a34797f6a8149b326d3a47181c1e
Adrian Grzemski <adrian.grzemski@gmail.com>
2025-08-11 21:27:30 +0200

    Refactor input handling: implement readUntilTerminator function for improved keyboard input processing and remove unused bufio code

--- app/main.go
+++ app/main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
+	"atomicgo.dev/keyboard"
+	"atomicgo.dev/keyboard/keys"
 	"github.com/codecrafters-io/shell-starter-go/commands"
 )
+
+func readUntilTerminator() (string, keys.KeyCode) {
+	var input strings.Builder
+
+	var lastKey keys.KeyCode
+
+	keyboard.Listen(func(key keys.Key) (stop bool, err error) {
+		switch lastKey = key.Code; lastKey {
+		case keys.Enter, keys.Tab, keys.Up, keys.Down:
+			return true, nil
+		case keys.CtrlC:
+			return true, nil
+		default:
+			if key.String() != "" {
+				input.WriteString(key.String())
+				// fmt.Print(key.String())
+			}
+		}
+
+		return false, nil
+	})
+
+	return input.String(), lastKey
+}
 
 func main() {
 	for {
@@ -14,26 +40,18 @@ func main() {
 			panic(err)
 		}
 
-		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
-		if err != nil {
-			panic(err)
-		}
+		// input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		// if err != nil {
+		// 	panic(err)
+		// }
 
-		if input == "\n" {
+		input, _ := readUntilTerminator()
+
+		if input == "" {
 			continue
 		}
 
 		status := commands.ExecCommand(input)
-
-		// log.Println(status)
-
-		// if status.Stdout != nil {
-		// 	fmt.Print(string(status.Stdout))
-		// }
-
-		// if status.Failed() {
-		// 	fmt.Println(status.Error())
-		// }
 
 		if exit, code := status.Exit(); exit {
 			os.Exit(code)
